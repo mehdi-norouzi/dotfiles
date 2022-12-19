ce242e82b8630822d16584a30dc4df40748e6e36
mehdi-norouzi <mehdeenoroozi@gmail.com>
2022-12-19 08:21:05 +0330

    nvim: add numbers to harpoon shortcuts

--- .config/nvim/lua/mehdi/mappings/init.lua
+++ .config/nvim/lua/mehdi/mappings/init.lua
@@ -75,6 +75,10 @@ map("n", "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", opts)
 map("n", "<leader>hl", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", opts)
 map("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", opts)
 map("n", "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", opts)
+map("n", "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", opts)
+map("n", "<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", opts)
+map("n", "<leader>h3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", opts)
+map("n", "<leader>h4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", opts)
 
 -- netrw
 map("n", "<leader>fm", "<cmd>Ex<cr>", opts)
